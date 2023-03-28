3cb6de2846e96c4a8b3e80b8ed0bc5e92f40a163
unknown <alcidesfreire@github.com>
2023-03-27 21:32:23 -0400

    fourth commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,16 +9,21 @@ namespace FirstSolution.userInterface.Console.ConsoleApp
 {
     class Program
     {
-        static Pessoa[] listaPessoa = new Pessoa[10];
+        static List<Pessoa> listaPessoa = new List<Pessoa>();
+        static List< TipoAtividade> listaAtividade = new List<TipoAtividade>();
+        static List<AtividadeComplementar> listaAtividadeComplementar = new List<AtividadeComplementar>();
+
         public static void Main(string[] args)
         {
             while (true)
             {
-                System.Console.WriteLine("ola, digite a ação 1-cadastro de aluno 2-cadastro de atividade  3-alt 4-exc 5-const 6-sair");
+                System.Console.WriteLine("ola, digite a ação 1-cadastro de aluno 2-cadastro de atividade  3-alt aluno 4-alt atividade 5-aluno exc 6-atividade exc " +
+                    "7- ler atividades 8-ler alunos 9-sair");
                 int op = Int32.Parse(System.Console.ReadLine());
 
                 switch (op)
                 {
+                    //cadastro de alunos
                     case 1:
                         System.Console.WriteLine("digite o id do aluno");
                         int pessoaID = Int32.Parse(System.Console.ReadLine());
@@ -27,83 +32,148 @@ namespace FirstSolution.userInterface.Console.ConsoleApp
                         System.Console.WriteLine("digite a data de nascimento:");
                         DateTime DataNascimento = DateTime.Parse(System.Console.ReadLine());
                         Pessoa p = new Pessoa(pessoaID, Nome, DataNascimento);
+                        listaPessoa.Add(p);
 
-                        for (int i = 0; i < listaPessoa.Length; i++)
-                        {
-                            if (listaPessoa[i] == null)
-                            {
-                                listaPessoa[i] = p;
-                                break;
-                            }
-                        }
                         break;
 
-                        case 2:
-                         TipoAtividade[] atividade = new TipoAtividade[10];
+                    //cadastro de atividades
+                    case 2:
+
                         System.Console.WriteLine("digite o id da atividade:");
                         int TipoAtividadeID = Int32.Parse(System.Console.ReadLine());
                         System.Console.WriteLine("digite a descrição:");
                         string Descricao = System.Console.ReadLine();
                         TipoAtividade t = new TipoAtividade(TipoAtividadeID, Descricao);
-                        for(int i = 0; i< atividade.Length; i++) 
-                        {
-                            if (atividade[i] == null) 
-                            {
-                                atividade[i] = t;
-                                break;
-                            }
-                        } break;
+                      listaAtividade.Add(t);
+                        break;
 
+                        //cadastro de atividade complementar
+                    case 10:
+                        System.Console.WriteLine("digite o id da atividade complementar:");
+                        int AtividadeComplementarID = Int32.Parse(System.Console.ReadLine());
+                        System.Console.WriteLine("digite a data da atividade:");
+                        DateTime data = DateTime.Parse(System.Console.ReadLine());
+                        
+                        System.Console.WriteLine("digite o id do aluno:");
+                        int idPessoa = Int32.Parse(System.Console.ReadLine());
+                        var aluno = GetPessoa(idPessoa);
 
+                        System.Console.WriteLine("digite o id do tipo de atividade:");
+                        int idTipoAtividade = Int32.Parse(System.Console.ReadLine());
+                        var tipoAtividade = GetTipoAtividade(idTipoAtividade);
+
+                        System.Console.WriteLine("digite a instituição");
+                        string instituicao = System.Console.ReadLine();
+                        System.Console.WriteLine("digite o ano de formação");
+                        int anoFormacao = Int32.Parse(System.Console.ReadLine());
+
+                        AtividadeComplementar a = new AtividadeComplementar(AtividadeComplementarID, data, instituicao, aluno, tipoAtividade,anoFormacao);
+                        listaAtividadeComplementar.Add(a);
+
+                        break;  
+
+                    //alteracao de alunos
                     case 3:
 
                         System.Console.WriteLine("digite o id do aluno a alterar:");
-                        int idAlterar = Int32.Parse(System.Console.ReadLine());
+                        int idAlunoAlterar = Int32.Parse(System.Console.ReadLine());
+                        var alunoAlterar = GetPessoa(idAlunoAlterar);
+                        if (alunoAlterar == null) continue;
 
-                        for (int i = 0;i < listaPessoa.Length; i++) 
-                        {
-                            if (listaPessoa[i].PessoaID == idAlterar) 
-                            {
-                                System.Console.WriteLine("digite o novo nome:");
-                                string novoNome = System.Console.ReadLine();
-                                System.Console.WriteLine("digite o novo ano:");
-                                DateTime novaData = DateTime.Parse(System.Console.ReadLine());
-                                listaPessoa[i].DataNascimento = novaData;
-                                listaPessoa[i].Nome = novoNome;
-                                break;
-                            }
-                        }
+                                                
+                        System.Console.WriteLine("digite o novo nome:");
+                        string novoNome = System.Console.ReadLine();
+                        System.Console.WriteLine("digite o novo ano:");
+                        DateTime novaData = DateTime.Parse(System.Console.ReadLine());
+                        alunoAlterar.DataNascimento = novaData;
+                        alunoAlterar.Nome = novoNome;
+
                         break;
 
-                        case 4:
+                    //alteracao de atividades
+                    case 4:
+                        System.Console.WriteLine("digite o id da atividade a alterar");
+                        int idAtividadeAlterar = Int32.Parse(System.Console.ReadLine());
+                        var atividadeAlterar = GetTipoAtividade(idAtividadeAlterar);
+                        if (atividadeAlterar == null) continue;
+                        System.Console.WriteLine("digite a nova descricao:");
+                        string novaDescricao = System.Console.ReadLine();
+                        atividadeAlterar.Descricao = novaDescricao;
+
+                      
+                        break;
+
+                    //alteração de atividade complementar
+                    case 11:
+                        System.Console.WriteLine("digite o id da atividade complementar a alterar");
+                        int idAtividadeComplementarAlterar = Int32.Parse(System.Console.ReadLine());
+                        var atividadeComplementarAlterar = GetAtividadeComplementar(idAtividadeComplementarAlterar);
+                        if (atividadeAlterar == null) continue;
+                        System.Console.WriteLine("digite a");
+                        
+
+                    //excluir alunos
+                    case 5:
                         System.Console.WriteLine("digite o id do aluno a ser excluido:");
-                        int idExcluir = Int32.Parse(System.Console.ReadLine());
+                        int idAlunoExcluir = Int32.Parse(System.Console.ReadLine());
+                        var alunoExcluir = GetPessoa(idAlunoExcluir);
+                        listaPessoa.Remove(alunoExcluir);                        
+                        break;
 
-                        for(int i = 0; i < listaPessoa.Length;i++) 
+                    //excluir atividades
+                    case 6:
+                        System.Console.WriteLine("digite o id da atividade a ser excluida");
+                        int idAtividadeExcluir = Int32.Parse(System.Console.ReadLine());
+                        var atividadeExcluir = GetTipoAtividade(idAtividadeExcluir);
+                        listaAtividade.Remove(atividadeExcluir);
+                      
+                        break;
+
+                    //ler atividades
+                    case 7:
+                        foreach(var t2 in listaAtividade)
                         {
-                            if (listaPessoa[i].PessoaID == idExcluir) 
-                            {
-                                listaPessoa[i] = null;
+                            System.Console.WriteLine("id: " + t2.TipoAtividadeID + "descricao: " + t2.Descricao);
 
-                            }
-                           
-                        }    break;
+                        }
+                      
 
-                        case 5:
-                        for(int i = 0; i< listaPessoa.Length ; i++) 
+                        break;
+
+                    //ler alunos
+                    case 8:
+                        foreach(var p2 in listaPessoa)
                         {
-                            Pessoa p2 = listaPessoa[i];
-                            if (p2 != null)
-                            {
-                                System.Console.WriteLine("id: " + p2.PessoaID + "nome: "+ p2.Nome + "data de nascimento" + p2.DataNascimento);
-                            }
-                        } break;
-                        case 6:
+                            System.Console.WriteLine("id: " + p2.PessoaID + "nome: " + p2.Nome + "data de nascimento" + p2.DataNascimento);
+                        }
+                        
+                        break;
+
+                    //sair
+                    case 9:
                         Environment.Exit(0);
                         break;
-                }  
+                }
             }
         }
-        
+
+        private static Pessoa GetPessoa(int pessoaId)
+        {
+            var pessoa = listaPessoa.Where(x => x.PessoaID == pessoaId).First();
+            return pessoa;
+        }
+
+        private static TipoAtividade GetTipoAtividade(int tipoAtividadeId)
+        {
+            var atividade = listaAtividade.Where(x => x.TipoAtividadeID == tipoAtividadeId).First();
+            return atividade;
+          
+        }
+
+        private static AtividadeComplementar GetAtividadeComplementar(int AtividadeComplementarId)
+        {
+            var complemento = listaAtividadeComplementar.Where(x => x.AtividadeComplementarId == AtividadeComplementarId).First();
+            return complemento;
+        }
     }
 }
--- First.Solution.Domain.ProjectDomain/AtividadeComplementar.cs
+++ First.Solution.Domain.ProjectDomain/AtividadeComplementar.cs
@@ -14,6 +14,19 @@ namespace First.Solution.Domain.ProjectDomain
         public TipoAtividade Tipo { get; set; }
         public string Instituicao { get; set; }
         public int AnoFormacao { get; set; }
-       
+
+        public AtividadeComplementar(int AtividadeComplementarId, DateTime Data, string Instituicao, Pessoa aluno, TipoAtividade tipo, int AnoFormacao)
+        {
+            this.AtividadeComplementarId = AtividadeComplementarId;
+            this.Data = Data;
+            this.Aluno = aluno;
+            this.Tipo = tipo;
+            this.Instituicao = Instituicao; 
+            this.AnoFormacao = AnoFormacao;
+        }
+        public static TipoAtividade Parse(string t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
